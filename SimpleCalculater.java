4d5d81f52ad2685582a93abd0facb2c1e5335de8
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2024-01-14 12:41:08 +0530

    math operation
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ SimpleCalculater.java
@@ -0,0 +1,15 @@
+public class SimpleCalculater {
+    public static void main(String[] args) {
+        // variables
+        int a = 25;
+        int b = 45 ;
+        int sum = a+b ;
+        System.out.println(sum);
+        int diff = a-b;
+        System.out.println(diff);
+        int mul= a*b;
+        System.out.println(mul);
+        float div = b/a;
+        System.out.println(div);
+    }
+}
