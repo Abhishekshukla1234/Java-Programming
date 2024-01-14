ce74a231a72d97ba0abb053c7de1a914f0c975e6
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2024-01-14 14:37:06 +0530

    simple calculater
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ simplecalculater1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class simplecalculater1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println(sum);
+        int diff = a-b;
+        System.out.println(diff);
+        int mul = a*b;
+        System.out.println(mul);
+        int div = a/b;
+        System.out.println(div);
+        
+    }
+}  
+
