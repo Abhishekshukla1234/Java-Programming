bcd22f141671d4cf0580cf3e0668e47ce329cfd3
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2024-01-25 16:49:21 +0530

    find the average of 3 number with the help of make function
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ function5.java
@@ -0,0 +1,19 @@
+
+    import java.util.Scanner;
+
+public class function5 {
+    public static int Average(int a,int b,int c){
+        int avg = (a+b+c)/2;
+        return avg;
+    }
+ public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+    int c = sc.nextInt();
+
+    int avg = Average(a, b, c);
+    System.out.println("Average of 3 number is "+" "+avg);
+ }    
+}
+
