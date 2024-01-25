7d693d0fda2780133056805777c5665f1ea1a343
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2024-01-25 15:24:20 +0530

    product of two number
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ function3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class function3 {
+    public static int calculatemulti(int a, int b){
+        int multi = a*b;
+        return multi;
+    }
+        public static void main (String[] args){
+            Scanner sc = new Scanner(System.in);
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int multi = calculatemulti(a, b);
+            System.out.println("multiple of two number is"+" "+multi);
+            
+        }
+
+}
