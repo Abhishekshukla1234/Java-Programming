1281503ef92e7c6c7297c3d3a9904bd9a3bf9f2f
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2024-01-25 18:22:18 +0530

    print the sum of odd number
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ function6.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class function6 {
+    public static int calculateSum(int n ){
+        int sum = 0;
+        for (int i=1; i<=n; i++ ){
+            if(i%2!=0){
+                System.out.println(i);  
+               sum =sum +i;
+            }
+    
+            }
+            return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+       int sum = calculateSum(n);
+       System.out.println("sum of odd number is"+" "+sum);
+            
+        
+    }
+}
