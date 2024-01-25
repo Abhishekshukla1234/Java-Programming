ed83aa2e6fa63331bc3f2ceb15215dd6bb93746b
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2024-01-25 18:52:17 +0530

    greater value
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ function7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class function7 {
+
+    public static int greaterValve(int a, int b){
+        if(a>b ){
+            System.out.println("A is grester"+ a);
+        }
+        else{
+            System.out.println("B is greater"+" "+ b);
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        greaterValve(a, b);
+    }
+}
