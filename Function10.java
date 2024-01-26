746807af81a221ed669a7564fb0967504a14d290
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2024-01-26 16:27:42 +0530

    infinite loop using do while loop
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ Function10.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Function10 {
+    public static int InfinteLoop(int i){
+        do{
+            System.out.println(i);
+            i++;
+        }while(i>=0);
+        return i;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        InfinteLoop(i);
+    }
+}
