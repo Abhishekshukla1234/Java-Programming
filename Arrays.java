e161621bf4ff1d14e37dba8fb77a9048e2c20711
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2024-02-02 09:24:02 +0530

    Array print
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ Arrays.java
@@ -0,0 +1,14 @@
+public class Arrays {
+    public static void main(String[] args) {
+        
+        int  marks [] = new int[3];
+
+        marks[0]=98;//phy
+        marks[1]=97;//chem
+        marks[2]=99;//eng
+       for (int i=0; i<3; i++){
+        System.out.println(marks[i]);
+       }
+
+    }
+}
