422802cc0f28f870cb864f0718265fa268fada8f
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2024-02-02 08:15:31 +0530

    count the number of negative , positive, & zero.
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ function11.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class function11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        int  X = sc.nextInt();
+        System.out.println("Enter the integer(Program end if enter 0):");
+        int [] data = new int [X];
+        for (int i = 0; i<data.length; i++ ){
+            System.out.println("Enter the Number "+(i+1));
+            data[i]=sc.nextInt();
+        }
+
+         int count = 0;
+         int positive = 0;
+         int negative = 0;
+         for(int i: data){
+            if (i > 0) {
+                positive++;
+            }
+            else if(i <0){
+                negative++;
+            }
+            else if (i == 0){
+                count++;
+            }
+         }
+         System.out.println("Positive Number :"+" "+positive );
+         System.out.println( "negative Number :"+" "+negative );
+         System.out.println("Zero number :"+" "+count);
+            
+
+        }
+    }
+
+
+
+        
+    
+
