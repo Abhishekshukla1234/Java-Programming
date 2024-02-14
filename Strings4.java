9b6cff3902480bd6dd104e87ae6ca6e31bccb16f
Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>
2024-02-14 10:52:10 +0530

    print substring
    Signed-off-by: Abhishek Kumar Shukla <91322687+Abhishekshukla1234@users.noreply.github.com>

---
+++ Strings4.java
@@ -0,0 +1,10 @@
+public class Strings4 {
+    public static void main(String[] args) {
+        //substring
+        String sentence = "AbhishekShukla";
+        String name = sentence.substring(0,4);
+        System.out.println(name);
+
+    
+    }
+}
